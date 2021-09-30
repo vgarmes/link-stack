fd135e7fff420d247e2c2dace8de25a3799bdede
Victor Garcia Mestre <vgmestre@gmail.com>
2021-09-30 11:28:58 +0200

    installed express and basic app setup

--- server/app.js
+++ server/app.js
@@ -1,1 +1,13 @@
-console.log('link-stack API');
+const express = require('express');
+const app = express();
+
+const port = process.env.PORT || 5000;
+const start = async () => {
+  try {
+    app.listen(port, console.log(`Server is listening on port ${port}...`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
