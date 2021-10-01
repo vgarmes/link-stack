9c89fd6dff92a51e524054dfe433da5e0b68c9c0
Victor Garcia Mestre <vgmestre@gmail.com>
2021-10-01 11:54:33 +0200

    create function attachCookiesToResponse

--- server/utils/jwt.js
+++ server/utils/jwt.js
@@ -9,7 +9,17 @@ const createJWT = ({ payload }) => {
 
 const isTokenValid = ({ token }) => JSON.verify(token, process.env.JWT_SECRET);
 
+const attachCookiesToResponse = ({ res, user }) => {
+  const token = createJWT({ payload: user });
+
+  res.cookie('token', token, {
+    httpOnly: true,
+    expires: new Date(Date.now() + 1000 * 60 * 60 * 24),
+  });
+};
+
 module.exports = {
   createJWT,
   isTokenValid,
+  attachCookiesToResponse,
 };
