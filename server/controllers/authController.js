384834f362e4c6742a49410c085452769cd1d93a
Victor Garcia Mestre <vgmestre@gmail.com>
2021-10-01 10:28:13 +0200

    prevent role injection, default role is admin if it is first user registered

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,5 +1,20 @@
+const User = require('../models/User');
+const { StatusCodes } = require('http-status-codes');
+const CustomError = require('../errors');
+
 const register = async (req, res) => {
-  res.send('register user');
+  // check for duplicated email (though it's also set up as unique on the db)
+  const { email, name, password } = req.body;
+  const emailAlredyExists = await User.findOne({ email });
+  if (emailAlredyExists) {
+    throw new CustomError.BadRequestError('Email already exists');
+  }
+
+  // first registered user is an admin
+  const isFirstAccount = (await User.countDocuments({})) === 0;
+  const role = isFirstAccount ? 'admin' : 'user';
+  const user = await User.create({ email, name, password, role });
+  res.status(StatusCodes.CREATED).json({ user });
 };
 
 const login = async (req, res) => {
