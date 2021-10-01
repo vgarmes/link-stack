30943cd2c3d5d6b5130185791fdb79c965c89ed0
Victor Garcia Mestre <vgmestre@gmail.com>
2021-10-01 11:41:07 +0200

    fixed cookie-parser import

--- server/app.js
+++ server/app.js
@@ -21,7 +21,7 @@ const errorHandlerMiddleware = require('./middleware/error-handler');
 
 app.use(morgan('tiny'));
 app.use(express.json());
-app.use(cookieParser);
+app.use(cookieParser());
 
 app.get('/', (req, res) => {
   res.send('link-stack API');
