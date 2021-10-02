139a93811ef80cf32340074cee23910364c5fc73
Victor Garcia Mestre <vgmestre@gmail.com>
2021-10-02 15:31:26 +0200

    attachCookies method uses signed cookies

--- server/utils/jwt.js
+++ server/utils/jwt.js
@@ -15,6 +15,8 @@ const attachCookiesToResponse = ({ res, user }) => {
   res.cookie('token', token, {
     httpOnly: true,
     expires: new Date(Date.now() + 1000 * 60 * 60 * 24),
+    secure: process.env.NODE_ENV === 'production',
+    signed: true,
   });
 };
 
