69708f64afecc19a8ab3022bc7bf5bf3f7131ea9
Victor Garcia Mestre <vgmestre@gmail.com>
2021-10-04 20:51:58 +0200

    create authorizePermissions middleware function

--- server/middleware/authentication.js
+++ server/middleware/authentication.js
@@ -17,4 +17,15 @@ const authenticateUser = async (req, res, next) => {
   }
 };
 
-module.exports = { authenticateUser };
+const authorizePermissions = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      throw new CustomError.UnauthorizedError(
+        'Unauthorized to access this route'
+      );
+    }
+    next();
+  };
+};
+
+module.exports = { authenticateUser, authorizePermissions };
