48cfb7d593df1ce14dabd6284ab879997a933a49
Victor Garcia Mestre <vgmestre@gmail.com>
2021-10-04 20:53:16 +0200

    use auth middleware

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { authenticateUser } = require('../middleware/authentication');
+const {
+  authenticateUser,
+  authorizePermissions,
+} = require('../middleware/authentication');
 
 const {
   getAllUsers,
@@ -10,11 +13,13 @@ const {
   updateUserPassword,
 } = require('../controllers/userController');
 
-router.route('/').get(authenticateUser, getAllUsers);
+router
+  .route('/')
+  .get(authenticateUser, authorizePermissions('admin'), getAllUsers);
 
-router.route('/showMe').get(showCurrentUser);
+router.route('/showMe').get(authenticateUser, showCurrentUser);
 router.route('/updateUser').patch(updateUser);
-router.route('/updateUserPassword').patch(updateUserPassword);
+router.route('/updateUserPassword').patch(authenticateUser, updateUserPassword);
 
 // this has to be last route, otherwise it will get confused with previous routes
 router.route('/:id').get(authenticateUser, getSingleUser);
