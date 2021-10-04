f6a69656a86bd4eb1f1b134bf92a4f300ed8eece
Victor Garcia Mestre <vgmestre@gmail.com>
2021-10-04 22:26:12 +0200

    return before hashing password if it was not modified when calling 'save' method

--- server/models/User.js
+++ server/models/User.js
@@ -32,6 +32,8 @@ const UserSchema = new mongoose.Schema({
 
 // Hash password
 UserSchema.pre('save', async function () {
+  // hash password only if we updated it before calling 'save'
+  if (!this.isModified('password')) return;
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
 });
