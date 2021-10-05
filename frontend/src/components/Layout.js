030fe9cdb1ffb2677e1e50f5f6775ea8a7ac3b9e
Victor Garcia Mestre <vgmestre@gmail.com>
2021-10-05 18:30:39 +0200

    add navbar and wrap children in a styled component

--- frontend/src/components/Layout.js
+++ frontend/src/components/Layout.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import { Navbar } from './';
+import styled from 'styled-components';
+
+const StyledContent = styled.div`
+  padding-top: var(--nav-height);
+  min-height: 100vh;
+  height: 100%;
+  overflow-x: hidden;
+`;
 
 const Layout = ({ children }) => {
   return (
     <div>
-      <div>Layout</div>
-      {children}
+      <Navbar />
+      <StyledContent>{children}</StyledContent>
     </div>
   );
 };
