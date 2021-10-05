31f17a44b29e48d479ba6014b9eaf7fc541664df
Victor Garcia Mestre <vgmestre@gmail.com>
2021-10-05 18:29:38 +0200

    created login and register pages

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,3 +1,6 @@
 import Home from './Home';
+import Login from './Login';
+import Register from './Register';
 import Error from './Error';
-export { Home, Error };
+
+export { Home, Login, Register, Error };
