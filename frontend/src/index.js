fa084baf048c7f0d48e740b9d54cff5d7dc8495b
Victor Garcia Mestre <vgmestre@gmail.com>
2021-10-05 12:05:41 +0200

    use GlobalStyle

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,10 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import GlobalStyle from './styles/GlobalStyle';
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
+      <GlobalStyle />
       <App />
     </BrowserRouter>
   </React.StrictMode>,
