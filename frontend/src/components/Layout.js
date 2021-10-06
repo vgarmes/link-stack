42aea16b812b16821379266108d8e386fe18034e
Victor Garcia Mestre <vgmestre@gmail.com>
2021-10-06 14:16:54 +0200

    added padding to layout

--- frontend/src/components/Layout.js
+++ frontend/src/components/Layout.js
@@ -2,11 +2,21 @@ import React from 'react';
 import { Navbar } from './';
 import styled from 'styled-components';
 
-const StyledContent = styled.div`
-  padding-top: var(--nav-height);
-  min-height: 100vh;
-  height: 100%;
+const StyledContent = styled.main`
+  margin-top: var(--nav-height);
   overflow-x: hidden;
+  min-height: 100vh;
+  padding: 0 48px;
+  @media (max-width: 1080px) {
+    padding: 0 24px;
+  }
+  @media (max-width: 768px) {
+    padding: 0 12px;
+  }
+  @media (max-width: 480px) {
+    padding: 0 6px;
+  }
+  transition: var(--transition);
 `;
 
 const Layout = ({ children }) => {
