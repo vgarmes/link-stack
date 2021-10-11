9b155ec94dce80933073099eae6c65f3f87b0738
Victor Garcia Mestre <vgmestre@gmail.com>
2021-10-11 10:16:36 +0200

    add dashboard route and wrap it in a private route

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-import { Home, Login, Register, Error } from './pages';
+import { Home, Login, Register, Error, Dashboard, PrivateRoute } from './pages';
 import { Layout } from './components';
 
 function App() {
@@ -18,6 +18,9 @@ function App() {
           <Route path="/register">
             <Register />
           </Route>
+          <PrivateRoute path="/dashboard">
+            <Dashboard />
+          </PrivateRoute>
           <Route path="*">
             <Error />
           </Route>
