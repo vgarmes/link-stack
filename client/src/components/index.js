80cfb8e8392b6fb93a24b682d85923ac6441098f
Victor Garcia Mestre <vgmestre@gmail.com>
2021-10-11 10:16:18 +0200

    export missing components

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -2,4 +2,6 @@ import Layout from './Layout';
 import Navbar from './Navbar';
 import Hero from './Hero';
 import Alert from './Alert';
-export { Layout, Navbar, Hero, Alert };
+import StyledForm from './StyledForm';
+import FormRow from './FormRow';
+export { Layout, Navbar, Hero, Alert, StyledForm, FormRow };
