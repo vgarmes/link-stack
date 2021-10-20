8d4e0be1bb3d7a927d277871ae2acaa1bb2150c4
Victor Garcia Mestre <vgmestre@gmail.com>
2021-10-20 10:58:00 +0200

    add verification properties to user schema

--- models/User.js
+++ models/User.js
@@ -35,6 +35,12 @@ const UserSchema = new mongoose.Schema({
     enum: ['admin', 'user'],
     default: 'user',
   },
+  verificationToken: String,
+  isVerified: {
+    type: Boolean,
+    default: false,
+  },
+  verified: Date,
 });
 
 // Hash password
