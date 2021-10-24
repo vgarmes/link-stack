6fa0a73a88a2db81229b477834092659a67e71c6
Victor Garcia Mestre <vgmestre@gmail.com>
2021-10-24 22:51:58 +0200

    create send reset pwd util

---
+++ utils/sendResetPasswordEmail.js
@@ -0,0 +1,16 @@
+const sendEmail = require('./sendEmail');
+
+const sendResetPasswordEmail = async ({ username, email, token, origin }) => {
+  const resetURL = `${origin}/user/reset-password?token=${token}&email=${email}`;
+  const message = `<p>Please reset password by clicking on the following link: 
+  <a href="${resetURL}">Reset Password</a></p>`;
+
+  return sendEmail({
+    to: email,
+    subject: 'Reset passord',
+    html: `<h4>Hello, ${username}</h4>
+    ${message}`,
+  });
+};
+
+module.exports = sendResetPasswordEmail;
