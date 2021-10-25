33c0605dd91053c1b2646cba6a4b5cc6a71f7de2
Victor Garcia Mestre <vgmestre@gmail.com>
2021-10-25 14:02:12 +0200

    use Button component

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,32 +1,18 @@
 import React from 'react';
-import styled from 'styled-components';
+//import styled from 'styled-components';
 import axios from 'axios';
 import useLocalState from '../hooks/useLocalState';
 import { useSessionContext } from '../context/session-context';
-import { Alert, StyledForm, FormRow } from '../components';
+import {
+  SectionCenter,
+  Button,
+  Alert,
+  StyledForm,
+  FormRow,
+} from '../components';
 import { Link, useHistory } from 'react-router-dom';
 import { Formik, Form, Field } from 'formik';
 import * as yup from 'yup';
-
-const StyledContainer = styled.div`
-  padding-top: 32px;
-  max-width: 500px;
-  margin: 0 auto;
-  h1 {
-    padding-bottom: 48px;
-  }
-  .login-link {
-    display: block;
-    width: 100%;
-    text-align: center;
-    font-size: 14px;
-    color: var(--grey-500);
-
-    &:hover {
-      text-decoration: underline;
-    }
-  }
-`;
 
 const validationSchema = yup.object({
   email: yup.string().required(),
@@ -59,7 +45,7 @@ const Login = () => {
   };
 
   return (
-    <StyledContainer>
+    <SectionCenter>
       <h1>Sign in to your Linkstack account</h1>
       {alert.show && <Alert type={alert.type}>{alert.text}</Alert>}
       <Formik
@@ -69,7 +55,7 @@ const Login = () => {
         validationSchema={validationSchema}
         onSubmit={handleSubmit}
       >
-        {({ values, isSubmitting, isValid }) => (
+        {({ isSubmitting, isValid }) => (
           <StyledForm as={Form} isDisabled={[isSubmitting, success]}>
             <FormRow label="email">
               <Field type="email" name="email" />
@@ -78,20 +64,22 @@ const Login = () => {
               <Field type="password" name="password" />
             </FormRow>
 
-            <button
+            <Button
               type="submit"
-              className="btn-small submit-btn"
               disabled={!isValid || isSubmitting}
+              size="md"
+              colorScheme="primary"
+              isFullWidth={true}
             >
               {isSubmitting ? 'Submitting...' : 'Log in'}
-            </button>
+            </Button>
           </StyledForm>
         )}
       </Formik>
       <Link to="/register" className="login-link">
         Don't have an account?
       </Link>
-    </StyledContainer>
+    </SectionCenter>
   );
 };
 
