094293dce38c73bffd33ee76556363f3c4d4c9ea
Victor Garcia Mestre <vgmestre@gmail.com>
2021-10-25 14:22:57 +0200

    removed button size styles

--- client/src/styles/GlobalStyle.js
+++ client/src/styles/GlobalStyle.js
@@ -45,64 +45,6 @@ button:disabled {
   background: var(--grey-200);
   color: var(--grey-400);
 }
-
-.btn-small {
-  height: 32px;
-  padding: 0 16px;
-  border-radius: 8px;
-  cursor: pointer;
-  width: auto;
-  position: relative;
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  align-items: center;
-  color: var(--color-background);
-  background: var(--color-secondary);
-  font-weight: bold;
-  text-transform: uppercase;
-  letter-spacing: var(--letterSpacing);
-}
-
-.btn-big {
-  height: 64px;
-  padding: 0 32px;
-  border-radius: 16px;
-  cursor: pointer;
-  width: auto;
-  position: relative;
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  align-items: center;
-  color: var(--color-background);
-  background: var(--color-primary);
-  font-weight: bold;
-  text-transform: uppercase;
-  font-size: 20px;
-  letter-spacing: var(--letterSpacing);
-}
-
-.btn {
-  cursor:pointer;
-  color: var(--white);
-  background: var(--primary-500);
-  border: transparent;
-  border-radius: var(--borderRadius);
-  letter-spacing: var(--leterSpacing);
-  padding: 0.375rem 0.75rem;
-  box-shadow: var(--shadow-1);
-  transition: var(--transition);
-  text-transform: capitalize;
-  display: inline-block;
-}
-.btn:hover {
-  background: var(--primary-700);
-  box-shadow: var(--shadow-3);
-}
-.btn-block {
-  width: 100%;
-}
 `;
 
 export default GlobalStyle;
