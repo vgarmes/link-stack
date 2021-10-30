305965e1da73388459f6a36bc5f3b6429f984f30
Victor Garcia Mestre <vgmestre@gmail.com>
2021-10-30 21:01:10 +0200

    create middleware for uploads with multer

---
+++ middleware/image-upload.js
@@ -0,0 +1,33 @@
+const multer = require('multer');
+
+const dir = './tmp/';
+const maxSize = 1e6; // maximum file size in bytes
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, dir);
+  },
+  filename: (req, file, cb) => {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+    cb(null, file.fieldname + '-' + uniqueSuffix);
+  },
+});
+
+module.exports = multer({
+  storage,
+  fileFilter: (req, file, cb) => {
+    if (
+      file.mimetype == 'image/png' ||
+      file.mimetype == 'image/jpg' ||
+      file.mimetype == 'image/jpeg'
+    ) {
+      cb(null, true);
+    } else {
+      cb(null, false);
+      return cb(new Error('Only .png, .jpg and .jpeg format allowed!'));
+    }
+  },
+  limits: {
+    fileSize: maxSize,
+  },
+});
