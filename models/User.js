4a090087177392fbc3f6014e1c47e324948a378c
Victor Garcia Mestre <vgmestre@gmail.com>
2021-11-01 10:20:05 +0100

    add linkstack property with type LinkstackSchema

--- models/User.js
+++ models/User.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
+const LinkstackSchema = require('./linkstack-schema');
 
 const UserSchema = new mongoose.Schema({
   username: {
@@ -47,6 +48,7 @@ const UserSchema = new mongoose.Schema({
   passwordTokenExpirationDate: {
     type: Date,
   },
+  linkstack: LinkstackSchema,
 });
 
 // Hash password
