959a087d19d339050826b09b94b2704e06c47de5
Victor Garcia Mestre <vgmestre@gmail.com>
2021-11-08 13:11:09 +0100

    add avatar

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,15 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import { useSessionContext } from '../context/session-context';
+import axios from 'axios';
+import { Avatar } from '../components';
 
 const Dashboard = () => {
   const { user } = useSessionContext();
+  const [linkstack, setLinkstack] = useState();
+
+  const fetchLinkstack = async () => {
+    try {
+      const { data } = await axios.get(`/api/v1/linkstacks/${user.username}`);
+      if (data && data.linkstack) {
+        setLinkstack(data.linkstack);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchLinkstack();
+    // eslint-disable-next-line
+  }, []);
+
   return (
     <Wrapper>
       <h2>
         Hello there, <span>{user.username}</span>
       </h2>
       <p>This is your dashboard.</p>
+      <Avatar
+        src="https://randomuser.me/api/portraits/men/75.jpg"
+        alt="avatar"
+        size={96}
+      />
     </Wrapper>
   );
 };
@@ -19,7 +44,6 @@ const Wrapper = styled.div`
   justify-content: center;
   align-items: center;
   flex-direction: column;
-  height: 50vh;
   h2 span {
     text-transform: capitalize;
   }
