c0d56158035b4360d3947765988c36e63b8c0f02
Victor Garcia Mestre <vgmestre@gmail.com>
2021-11-11 11:53:10 +0100

    implemented button variants

--- client/src/components/buttons/Button.js
+++ client/src/components/buttons/Button.js
@@ -1,15 +1,64 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+
+const getStyleMixin = (props) => {
+  const { variant, colorScheme } = props;
+  let styleMixin = css`
+    color: var(--color-white);
+    background: ${({ theme }) => theme.colors[colorScheme]};
+    &:hover {
+      background: ${({ theme }) => theme.colors[colorScheme + '-600']};
+    }
+  `;
+
+  switch (variant) {
+    case 'solid':
+      return styleMixin;
+    case 'outline':
+      return (styleMixin = css`
+        color: ${({ theme }) => theme.colors[colorScheme + '-600']};
+        background: transparent;
+        border: 1px solid ${({ theme }) => theme.colors[colorScheme + '-600']};
+        &:hover {
+          background: ${({ theme }) => theme.colors[colorScheme + '-50']};
+        }
+      `);
+    case 'ghost':
+      return (styleMixin = css`
+        color: ${({ theme }) => theme.colors[colorScheme + '-600']};
+        background: transparent;
+        &:hover {
+          background: ${({ theme }) => theme.colors[colorScheme + '-50']};
+        }
+      `);
+    case 'link':
+      return (styleMixin = css`
+        color: ${({ theme }) => theme.colors[colorScheme + '-300']};
+        background: transparent;
+        &:hover {
+          text-decoration: underline;
+        }
+      `);
+    default:
+      return styleMixin;
+  }
+};
 
 const Wrapper = styled.button`
   ${({ size, theme }) => theme.mixins.buttons[size]}
 
-  background: ${({ colorScheme, theme }) => theme.colors[colorScheme]}
+  width: ${({ theme, isFullWidth, width }) =>
+    isFullWidth ? '100%' : theme.sizes[width]};
+
+  ${(props) => getStyleMixin(props)}
 `;
 
 Wrapper.defaultProps = {
   size: 'md',
   colorScheme: 'primary',
+  variant: 'solid',
+  isFullWidth: false,
+  width: 'auto',
 };
 
 const Button = ({ children, ...props }) => {
